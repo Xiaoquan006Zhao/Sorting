6c7cb649774e1bb047554a8ca5cdf40c217b8a0a
Xiaoquan Zhao <1009642031@qq.com>
2018-12-16 17:25:00 -0600

    Selection sort in java
    O(n^2) worst case

---
+++ selection_sort.java
@@ -0,0 +1,21 @@
+package sorting;
+
+public class selection_sort {
+
+	  public static int[] selectionSort(int[] toSort) {
+			 for (int i = 0; i < toSort.length-1; i++) {
+					// index for the "smallest" element
+					int smallest = i;
+					for (int j = i + 1; j < toSort.length; j++) {
+						  if (toSort[j] < toSort[smallest]) {
+								 smallest = j;
+						  }
+					}
+					// swap 
+					int temp = toSort[smallest];
+					toSort[smallest] = toSort[i];
+					toSort[i] = temp;
+			 }
+			 return toSort;
+	  }
+}
