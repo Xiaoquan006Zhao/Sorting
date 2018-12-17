5c110e183b4cd5d3091cb21a236c0f7dbd125484
Xiaoquan Zhao <1009642031@qq.com>
2018-12-16 18:30:25 -0600

    Insertion_sort in java
    O(n^2) worst case. Usually faster than selection sort

---
+++ Insertion_sort.java
@@ -0,0 +1,20 @@
+package sorting;
+
+
+public class Insertion_sort {
+
+	  public static int[] insertionSort(int[] toSort) {
+			 for (int i = 1; i < toSort.length; i++) {
+					int insert = toSort[i];
+					int j;
+					for (j = i; j >= 1 && toSort[j - 1] > insert; j--) {
+						  toSort[j] = toSort[j - 1];
+					}				
+					//check if swap is necessary
+					if (j != i)
+						  toSort[j] = insert;
+			 }
+			 return toSort;
+	  }
+	  
+}
