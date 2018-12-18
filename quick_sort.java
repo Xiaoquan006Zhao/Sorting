9cb45a9eb890d0716a8eabd149f3654103f87c2f
Xiaoquan Zhao <1009642031@qq.com>
2018-12-18 12:55:53 -0600

    quick_Sort

---
+++ quick_sort.java
@@ -0,0 +1,34 @@
+package sorting;
+
+
+//O(n*log(n) )
+public class quick_sort {
+
+	  public static void quickSort(int[] toSort, int start, int end) {
+			 if (start >= end)
+					return;
+
+			 int initial = start + 1;
+			 int terminal = end;
+			 int cursor = toSort[start]; //pick the first element as the cursor
+
+			 while (initial < terminal) {
+					while (initial < end && toSort[initial] <= cursor)
+						  initial++;
+					while (terminal >= start && toSort[terminal] > cursor)
+						  terminal--;
+
+					if (initial < terminal) {
+						  int temp = toSort[initial];
+						  toSort[initial] = toSort[terminal];
+						  toSort[terminal] = temp;
+					}	
+			 }
+			 toSort[start] = toSort[terminal];
+			 toSort[terminal] = cursor;
+			 
+			 quickSort(toSort, start, terminal - 1);
+			 quickSort(toSort, terminal + 1, end);
+	  }
+
+}
